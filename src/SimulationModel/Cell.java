c39897a08ce4e2de846f55d3a6333caa415f988f
TTmesb <emil.aberg@elev.ga.ntig.se>
2021-12-13 09:36:50 +0100

    den rör på sig

---
+++ src/SimulationModel/Cell.java
@@ -0,0 +1,38 @@
+package SimulationModel;
+
+import Shapes.Point;
+import Shapes.Shape;
+import Shapes.Sprite;
+
+/**
+ * This is a class
+ * Created 2021-11-15
+ *
+ * @author Magnus Silverdal
+ */
+public class Cell {
+    private int x;
+    private int y;
+
+    public Cell(int x, int y) {
+        this.x = x;
+        this.y = y;
+
+    }
+
+    public Shape getShape() {
+        return new Point(x,y);
+    }
+
+    public void update() {
+
+        if( this.x < 50) {
+            x++;
+        }
+
+    }
+
+    public Sprite getSprite() {
+        return null;
+    }
+}
---
+++ src/SimulationModel/SimulationModel.java
@@ -0,0 +1,32 @@
+package SimulationModel;
+
+import Shapes.Scene;
+import Shapes.Shape;
+import Shapes.Sprite;
+
+import java.util.ArrayList;
+
+/**
+ * This is a class
+ * Created 2021-10-18
+ *
+ * @author Magnus Silverdal
+ */
+public class SimulationModel {
+    Scene scene;
+    Cell c;
+    public SimulationModel() {
+        c = new Cell(2,2);
+    }
+    public void update() {
+        c.update();
+    }
+
+    public ArrayList<Shape> getShapes() {
+        ArrayList<Shape> shapes = new ArrayList<>();
+        shapes.add(c.getShape());
+        return shapes;
+    }
+
+
+}
